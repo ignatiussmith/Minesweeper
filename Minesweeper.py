4e38203b1e85277018f0c753ec049a40e6917ce9
Ignatius Smith <is116615@ohio.edu>
2020-08-19 16:27:55 -0400

    added ability for custom boards, user promting for board size

--- Minesweeper.py
+++ Minesweeper.py
@@ -5,16 +5,25 @@
 import random
 #Game will probably be a class
 #Board will probably be a class
-def MakeBoard(size): 
-	
-	if size == 'B':
-		#initialize the board, this prolly could be its own function
+def MakeBoard(): 
+	#need to add error handling to input, valid sizes and mines
+	size = input("Enter difficulty (B, I, A, C): ")
+	if size == 'B' or size == 'b':
 		board = initialize_board(10, 10)
 		produce_minefield(10, board)	
-	if size == 'I':
+	elif size == 'I' or size == 'i':
+		board = initialize_board(20, 20)
+		produce_minefield(40, board)
+	elif size == 'A' or size == 'a':
 		board = initialize_board(20, 40)
 		produce_minefield(100, board)
-		
+	elif size == 'C' or size == 'c':
+		rows = int(input("Enter no rows: "))
+		columns = int(input("Enter no columns: "))
+		mines = int(input("Enter no mines: "))
+		board = initialize_board(rows, columns)
+		produce_minefield(mines, board)
+			
 	return board;
 	
 def PrintBoard(board):
@@ -39,6 +48,7 @@ def produce_single_mine(p):
 def iterate_neighbors(row, column, board):
 	new_board = board;
 	#if its a valid index, and doesn't touch a mine, we iterate
+	#we want to update number of neighboring mines for numbered squares
 	if row + 1 < len(board):
 		if column + 1 < len(new_board[row]):
 			if new_board[row + 1][column + 1] != 'm':
@@ -73,7 +83,7 @@ def produce_minefield(no_mines, board):
 	board_avec_mines = board
 	no_placed_mines = 0
 	#iterate through squares, choose mine with probability mines/squares
-	prob_mine = no_mines / (len(board) * len(board[1]))
+	prob_mine = no_mines / (len(board) * len(board[0]))
 	for i in range(len(board)):
 		for j in range(len(board[i])):
 			if produce_single_mine(prob_mine) and no_placed_mines < no_mines:
@@ -81,8 +91,7 @@ def produce_minefield(no_mines, board):
 				no_placed_mines += 1
 				board_avec_mines = iterate_neighbors(i, j, board_avec_mines)
 				
-	#if we don't hit target no. mines, then yeet one in there
-		#chance to be chaotic with lying about mine no.
+	#if we don't hit target no. mines, then yeet one in there at random
 	while no_placed_mines < no_mines:
 		row = int(random.random() * len(board))
 		column = int(random.random() * len(board[0]))
@@ -90,12 +99,6 @@ def produce_minefield(no_mines, board):
 			board_avec_mines[row][column] = 'm'
 			no_placed_mines += 1
 			board_avec_mines = iterate_neighbors(row, column, board_avec_mines)
-
-	#when a mine is placed, iterate all surround squares (excl. mines) up
 	
-	#check for spaces with zero mines touching, and mark as such	
-	
-for i in range(5):	
-	PrintBoard(MakeBoard('B'))
-	print(' ')
-#PrintBoard(MakeBoard('I'))
+#MAIN PROGRAM	
+PrintBoard(MakeBoard())
